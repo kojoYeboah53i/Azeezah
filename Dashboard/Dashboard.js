f6cac36d82a92a12fe89eb13828072feb65eff9b
ZeeAzeez <azeezahaazeez@gmail.com>
2023-09-25 00:04:43 +0000

    completed order table using javascript

--- Dashboard/Dashboard.js
+++ Dashboard/Dashboard.js
@@ -16,7 +16,21 @@ closeBtn.addEventListener('click', () => {
 // code for changing theme
 themeToggler.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme-variables');
-
+// to show active button
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+// code for filling in order in table
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+                    <td>${order.productName}</td>
+                    <td>${order.productNumber}</td>
+                    <td>${order.paymentStatus}</td>
+                    <td class="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'pending' ? 'warning'
+                                : 'primary'}">${order.shipping}</td>
+                    <td class="primary">Details</td>
+                    `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
 })
