2a49a6749d144fde425e838acda5adda2c422d32
Isaac Kojo Yeboah <yeboah.determined.isaac@gmail.com>
2023-10-15 07:18:32 -0700

    update

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.listen(5584, () => {
+    console.log('Server started , http://localhost:5584');
+})
---
+++ backend/nodes.md
@@ -0,0 +1,9 @@
+let table = {
+    teams : {
+        name: "",
+        badge: "",
+        matchesPlayed: "",
+
+    },
+
+}
